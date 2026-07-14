640d69530895c0c678bc4614c609a9c3e3e3aa3a
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-07-14 19:08:45 +0200

    Added the hamburger menu and styled it to appear on screens with width of 786px and lower

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,52 +1,48 @@
-
-import logo from '../assets/images/logo.png'
-import { HugeiconsIcon } from '@hugeicons/react'
-import { Location03Icon } from '@hugeicons/core-free-icons'
-import { ShoppingCart02Icon } from '@hugeicons/core-free-icons'
+import {useState } from "react";
+import logo from "../assets/images/logo.png";
+import { HugeiconsIcon } from "@hugeicons/react";
+import {Location03Icon,ShoppingCart02Icon} from "@hugeicons/core-free-icons";
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
 
   return (
-
     <header className="header">
 
       <div className="logo">
         <img src={logo} alt="logo" />
       </div>
-      <div className='navLinks'>
-      <nav>
 
-        <a href="#">Home</a>
+      {/* Hamburger Button */}
+      <button className="hamburger" onClick={() => setOpen(!open)}>
+          ☰
+      </button>
 
-        <a href="#">Menu</a>
+      {/* Navigation */}
+      <div className={`navLinks ${open ? "show" : ""}`}>
 
-        <a href="#">Pages</a>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Menu</a>
+          <a href="#">Pages</a>
+          <a href="#">Blog</a>
+          <a href="#">Contact Us</a>
+        </nav>
 
-        <a href="#">Blog</a>
+        <div className="headerButtons">
 
-        <a href="#">Contact Us</a>
-
-      </nav>
-      
-
-      <div className="headerButtons">
-
-        <button className="iconButton"  onClick={() => alert("Our restaurant is located at 123 Healthy Street, Fresh City.")}>
-
+          <button className="iconButton" onClick={() =>alert("Our restaurant is located at 123 Healthy Street, Fresh City.")}>
             <HugeiconsIcon icon={Location03Icon} />
+          </button>
 
-        </button>
-
-        <button className="iconButton" onClick={() => alert("Your shopping cart is currently empty.")}>
+          <button className="iconButton" onClick={() => alert("Your shopping cart is currently empty.") }>
             <HugeiconsIcon icon={ShoppingCart02Icon} />
+          </button>
 
-        </button>
+        </div>
 
-      </div>
       </div>
 
     </header>
-
-  )
-
-}
+  );
+};
